bcf0fd4114802aa356d5bf0e1b58d939ecd5a034
Tom Adams <tom@dxw.com>
2015-07-20 09:27:59 -0400

    Increase sleep interval

--- tools/internal/data-restore.sh
+++ tools/internal/data-restore.sh
@@ -8,7 +8,7 @@ restore() {
   docker run --label=com.dxw.whippet=true --label=com.dxw.data=true --name=${1} -v /var/lib/mysql mysql /bin/true
 
   docker run -d --name=temp_mysql --volumes-from=${1} -e MYSQL_DATABASE=wordpress -e MYSQL_ROOT_PASSWORD=foobar mysql
-  sleep 15
+  sleep 30
   docker run -i --rm --link=temp_mysql:mysql mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD" "$MYSQL_ENV_MYSQL_DATABASE"' < backup/${1}.sql
 }
 
