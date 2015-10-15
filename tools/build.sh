0a603119c5acf9e3ead1141b0223c16b5ce3f1a5
Tom Adams <tom@dxw.com>
2015-10-15 08:49:59 -0400

    Fix the tools a bit

--- tools/build.sh
+++ tools/build.sh
@@ -3,4 +3,6 @@ set -xe
 
 boot2docker up
 `boot2docker shellinit`
+docker ps || boot2docker ssh sudo /etc/init.d/docker restart
+docker build --no-cache -t thedxw/workshop-base /workbench/src/git.dxw.net/workshop/base.git
 docker build --no-cache -t workshop /workbench/workshop
--- tools/run.sh
+++ tools/run.sh
@@ -3,5 +3,8 @@ set -xe
 
 boot2docker up
 `boot2docker shellinit`
+docker ps || boot2docker ssh sudo /etc/init.d/docker restart
+sleep 5
+
 docker rm workshop || docker attach workshop ||
 docker run -ti --name workshop -h workshop -e "DOCKER_HOST=$DOCKER_HOST" -e "DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY" -e DOCKER_CERT_PATH=/boot2docker-cert -v "$DOCKER_CERT_PATH:/boot2docker-cert:ro" -v /usr/local/bin/docker:/usr/bin/docker:ro -v /workbench:/workbench workshop
