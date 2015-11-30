d78d16a95a3d3b81681eb0dc19c7df83694860ed
Tom Adams <tom@dxw.com>
2015-11-30 16:53:18 -0500

    Remove tools/build.sh

--- tools/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -xe
-
-docker-machine start default || true
-eval "$(docker-machine env default)"
-
-docker build --no-cache -t thedxw/workshop-base /workbench/src/git.dxw.net/workshop/base.git
-docker build --no-cache -t workshop /workbench/workshop
