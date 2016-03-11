9e788d4d5caeb392e877522e017a392243d01ed0
Tom Adams <tom@dxw.com>
2016-03-11 08:47:29 -0500

    Allow specifying workshop image name

--- tools/run.sh
+++ tools/run.sh
@@ -2,8 +2,10 @@
 set -xe
 
 MACHINE=${1}
-if test X$MACHINE = X; then
-  echo "Usage: ${0} machine-name"
+IMAGE=${2}
+if test X${MACHINE} = X || test X${IMAGE} = X; then
+  echo "Usage: ${0} machine-name image-name"
+  echo "i.e. ${0} default thedxw/workshop"
   exit 1
 fi
 
@@ -14,5 +16,5 @@ if test X`docker inspect --format='{{.State.Running}}' workshop` = Xtrue; then
   exec docker attach workshop
 else
   docker rm workshop || true
-  exec docker run -ti --rm --name workshop --hostname workshop -e "DOCKER_HOST=$DOCKER_HOST" -e "DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY" -e DOCKER_CERT_PATH=/docker-cert -v "$DOCKER_CERT_PATH:/docker-cert:ro" -v /usr/local/bin/docker:/usr/bin/docker:ro -v /workbench:/workbench workshop
+  exec docker run -ti --rm --name workshop --hostname workshop -e "DOCKER_HOST=${DOCKER_HOST}" -e "DOCKER_TLS_VERIFY=${DOCKER_TLS_VERIFY}" -e DOCKER_CERT_PATH=/docker-cert -v "${DOCKER_CERT_PATH}:/docker-cert:ro" -v /usr/local/bin/docker:/usr/bin/docker:ro -v /workbench:/workbench ${IMAGE}
 fi
