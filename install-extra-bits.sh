aacf34a8d19af62ae839a83951bcd7e55ade031b
Tom Adams <tom@dxw.com>
2017-04-03 11:58:15 -0400

    Update pluginscan installation script

--- install-extra-bits.sh
+++ install-extra-bits.sh
@@ -10,12 +10,8 @@ LIB=/workbench/local/lib
 mkdir -p ${SRC} ${SHARE} ${BIN} ${LIB}
 
 # pluginscan
-git -C ${SRC} clone --quiet git@git.govpress.com:dxw/pluginscan.git pluginscan && \
-  sh -c "cd ${SRC}/pluginscan && gem build pluginscan.gemspec" && \
-  gem install --install-dir=${LIB}/rubygems ${SRC}/pluginscan/pluginscan-*.gem
-  echo '#!/bin/sh' > ${BIN}/pluginscan && \
-  echo 'GEM_PATH=${GEM_PATH}:'${LIB}'/rubygems exec '${LIB}'/rubygems/bin/pluginscan ${@}' >> ${BIN}/pluginscan && \
-  chmod 755 ${BIN}/pluginscan
+gem sources -a https://gems.dxw.net/ && \
+  GEM_HOME=/workbench/local/ruby gem install pluginscan
 
 # pupdate
 git -C ${SRC} clone --quiet git@git.dxw.net:plugin-updater && \
