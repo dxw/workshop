34c745f183e1f9da05b25d7723cbcb06739a1535
Tom Adams <tom@dxw.com>
2017-06-30 09:59:50 -0400

    Add log-tail

--- install-extra-bits.sh
+++ install-extra-bits.sh
@@ -18,3 +18,10 @@ git -C ${SRC} clone --quiet git@git.dxw.net:plugin-updater && \
   cp -r ${SRC}/plugin-updater ${SHARE}/pupdate && \
   /bin/echo -e '#!/bin/sh\nset -e\ncd '${SHARE}'/pupdate/updating\n./update.sh $1 git@git.dxw.net:wordpress-plugins/$1\ncd -' > ${BIN}/pupdate && \
   chmod 755 ${BIN}/pupdate
+
+# log-tail
+git -C ${SRC} clone --quiet git@git.govpress.com:dxw/log-tail && \
+  git -C ${SRC} clone --quiet git@git.govpress.com:dxw/log-tail-settings-generator && \
+  composer --working-dir=${SRC}/log-tail install && \
+  ln -s ../log-tail-settings-generator/settings.php ${SRC}/log-tail/ && \
+  ln -s ${SRC}/log-tail/bin/log-tail ${BIN}/
