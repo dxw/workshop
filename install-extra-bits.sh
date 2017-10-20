4ffb2c1b14fdf8d82ee8128a67131ab09c826f53
Tom Adams <tom@dxw.com>
2017-10-20 15:16:33 -0400

    Remove pupdate as plugins are hosted elsewhere now

--- install-extra-bits.sh
+++ install-extra-bits.sh
@@ -13,12 +13,6 @@ mkdir -p ${SRC} ${SHARE} ${BIN} ${LIB}
 gem sources -a https://gems.dxw.net/ && \
   GEM_HOME=/workbench/local/ruby gem install rake pluginscan
 
-# pupdate
-git -C ${SRC} clone --quiet git@git.dxw.net:plugin-updater && \
-  cp -r ${SRC}/plugin-updater ${SHARE}/pupdate && \
-  /bin/echo -e '#!/bin/sh\nset -e\ncd '${SHARE}'/pupdate/updating\n./update.sh $1 git@git.dxw.net:wordpress-plugins/$1\ncd -' > ${BIN}/pupdate && \
-  chmod 755 ${BIN}/pupdate
-
 # log-tail
 git -C ${SRC} clone --quiet git@git.govpress.com:dxw/log-tail && \
   git -C ${SRC} clone --quiet git@git.govpress.com:dxw/log-tail-settings-generator && \
